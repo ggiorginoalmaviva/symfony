547b9fb6e885c7a43276ad8188b72c4e9dacd782
Gregorio Giorgino (AlmaViva DigitalTech) <g.giorgino@almaviva.it>
2024-10-30 16:19:34 +0100

    Added php cs fixer

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,17 +1,19 @@
 <?php
-    
-    declare(strict_types=1);
-    
-    namespace App\Controller;
-    
-    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-    use Symfony\Component\HttpFoundation\Response;
-    use Symfony\Component\Routing\Attribute\Route;
-    
-    class MainController extends AbstractController {
-        #[/* Symfony\Component\Routing\Attribute\ */Route('/')]
-        public function index(): Response {
-             return new \Symfony\Component\HttpFoundation\Response("<h1>hello world!</h1>");
-            //return $this->render('main/index.html.twig');
-        }
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[/* Symfony\Component\Routing\Attribute\ */ Route('/')]
+    public function index(): Response
+    {
+        return new Response('<h1>hello world!</h1>');
+        // return $this->render('main/index.html.twig');
     }
+}
